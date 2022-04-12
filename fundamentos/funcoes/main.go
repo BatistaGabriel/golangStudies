e7968aa8188b4f8a9a65f2f559b8a39c6bb78065
Gabriel Batista <gabriel_batista@trimble.com>
2022-04-12 14:26:14 -0300

    oitava aula: conhecendo funcoes

---
+++ fundamentos/funcoes/main.go
@@ -0,0 +1,15 @@
+package main
+
+/*
+	para executar funções que a main separada
+	navegue por linha de comando e depois execute o go run
+	no caso para este cenário ficaria
+
+	cd fundamentos/funcoes
+	go run *.go
+*/
+
+func main() {
+	resultado := somar(3, 4)
+	imprimir(resultado)
+}
